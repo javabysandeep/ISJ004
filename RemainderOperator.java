e831b89ad056fbd7903500063e574b15b5a8b65e
sandeep bhingle <sandeep.bhingle@itshaala.com>
2023-07-26 11:09:21 +0530

    arithmetic, concatenation, shorthand, assignment operators

---
+++ RemainderOperator.java
@@ -0,0 +1,19 @@
+public class RemainderOperator {
+
+	public static void main(String[] args){
+		
+		System.out.println(123 % 10);//3
+		System.out.println(123 /10);//12
+
+		System.out.println(12 / 10);//1
+		System.out.println(12 % 10);//2
+
+		System.out.println(1 % 10);//1
+		System.out.println(1 / 10); //0
+
+		System.out.println(0 % 10000);//0
+		System.out.println(0 / 10000); //0
+		
+
+	}	
+}
