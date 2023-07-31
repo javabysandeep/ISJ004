71b65f40a72ff1eb8010d0f31a3765d3c7041056
sandeep bhingle <sandeep.bhingle@itshaala.com>
2023-07-31 11:43:08 +0530

    Logical Operators

--- ComparisionOperator1.java
+++ ComparisionOperator1.java
@@ -7,6 +7,7 @@ public class ComparisionOperator1 {
 		//System.out.println(false=false); //CTE
 		
 		
+		
 
 	}	
 }
---
+++ PureLogicalOperator3.java
@@ -0,0 +1,21 @@
+public class PureLogicalOperator3 {
+
+	public static void main(String[] args){
+		int number = 2023;
+		System.out.println( number%400==0);//false 
+		System.out.println( number%100!=0);//true
+		System.out.println( number%4==0);//false		
+		
+		System.out.println( (number%4==0) || (number%100!=0) && (number%4==0) );//false		
+		
+		System.out.println( 
+					(number%4==0) || 
+						( (number%100!=0) && (number%4==0)) 
+				
+				);//false		
+	
+	
+		
+
+	}	
+}
